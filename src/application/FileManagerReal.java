02774b24b1dd6050f9aec1c04a34b89b51dd3565
terabyte-13 <giacomo200303@gmail.com>
2025-02-14 17:04:37 +0100

    rimozione smell 4

--- src/application/FileManagerReal.java
+++ src/application/FileManagerReal.java
@@ -16,11 +16,11 @@ public class FileManagerReal extends FileManager {
 	Alert fileExistsAlert = new Alert(AlertType.CONFIRMATION);
 	ButtonType overwrite = new ButtonType("Sovrascrivi");
 	ButtonType cancel = new ButtonType("Annulla");
+	
+	File outputFile;
 
 	int save(String data, String outputPath, String fileName, Boolean confirmOverwrite) {
 		try {
-			File outputFile;
-			FileWriter writer;
 			// creazione cartelle e file ------------------------------------------------------------------------
 			if(!outputPath.equals("")) {
 				outputFile = new File(outputPath);
@@ -29,36 +29,41 @@ public class FileManagerReal extends FileManager {
 			}else {
 				outputFile = new File(fileName);
 			}
-//              vv restituisce false anche se per qualche motivo OutputFile.exists() restituisce null
-			if(Boolean.TRUE.equals(outputFile.exists()) || confirmOverwrite == true) {
+//         				 vv restituisce false anche se per qualche motivo OutputFile.exists() restituisce null.			
+			if(Boolean.FALSE.equals(outputFile.exists()) || confirmOverwrite) {
 				outputFile.createNewFile();
-				System.out.printf("<FileMan> File creato: %s%n", outputFile.getName());
+				System.out.printf("<FileMan> File creato: %s\n", outputFile.getName());
 			} else { // popup file già esistente --------------------------------------------------------------
 				System.out.println("<FileMan> File gia' esistente");
 				fileExistsAlert.setTitle("File già esistente");
 				fileExistsAlert.setHeaderText("Questo file è già presente. Sovrascrivere?");
-				fileExistsAlert.setContentText(outputPath + File.separator + fileName);
+				fileExistsAlert.setContentText(outputPath + "/" + fileName);
 				fileExistsAlert.getButtonTypes().setAll(overwrite,cancel);
 				Optional<ButtonType> choice = fileExistsAlert.showAndWait();
-				if(choice.isPresent() && choice.get() == overwrite) {
-					
-				}
+
 				if(choice.isPresent() && choice.get() == cancel) {
 					System.out.println("<FileMan> Creazione file annullata");
 					return 0;
 				}
 				}
-			// scrittura sul file ------------------------------------------------------------------------
-			writer = new FileWriter(outputFile);
-			writer.write(data);
-			writer.close();
-			System.out.printf("<FileMan> File scritto: %s\n", outputFile.getName());
-			// ------------------------------------------------------------------------------------------
 		}catch (IOException e) {
 			System.out.println("<FileMan> Errore nella creazione del file");
 			e.printStackTrace();
 			return 0;
 		}
+		
+		//creando il writer fra le parentesi del try, viene chiuso il writer anche in caso di eccezione
+		try(FileWriter writer = new FileWriter(outputFile)){
+			// scrittura sul file ------------------------------------------------------------------------
+			writer.write(data);
+			writer.close();
+			System.out.printf("<FileMan> File scritto: %s\n", outputFile.getName());
+		}catch (IOException e){
+			System.out.println("<FileMan> Errore nella creazione del file");
+			e.printStackTrace();
+			return 0;
+		}
+		
 		return 1;
 	}
 	
@@ -76,7 +81,7 @@ public class FileManagerReal extends FileManager {
 			data = data + reader.nextLine() + "\n";
 		}
 		reader.close();
-		System.out.printf("<FileMan> File letto: %s\n", inputFile.getName());
+		System.out.printf("<FileMan> File letto: %s%n", inputFile.getName());
 		// --------------
 		} catch (FileNotFoundException e) {
 			System.out.println("<FileMan> Errore nell'apertura del file");
